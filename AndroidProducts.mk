a7287eb58ecb448e912e85e90132cc737ac51326
MRMDprjkt° <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-09-09 07:05:57 +0700

    Nusantara opjektif

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/qassa_lavender.mk
+    $(LOCAL_DIR)/nad_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    qassa_lavender-user \
-    qassa_lavender-userdebug \
-    qassa_lavender-eng
+    nad_lavender-user \
+    nad_lavender-userdebug \
+    nad_lavender-eng

69bc875c5c0f479985c142662986c62d78441dfd
MRMDprjkt° <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-10-03 11:59:11 +0700

    Update nad_lavender.mk

--- nad_lavender.mk
+++ nad_lavender.mk
@@ -30,12 +30,14 @@ PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=lavender \
-    PRODUCT_NAME=lavender
+    PRODUCT_DEVICE="lavender" \
+    PRODUCT_NAME="lavender" \
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGMIXM release-keys"
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+BUILD_FINGERPRINT :="xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGMIXM:user/release-keys"
 
 
 
